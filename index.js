c9487166df2b864692269f421070e13d44486065
Amit Chakraborty <34010291+amitchakraborty123@users.noreply.github.com>
2019-08-05 14:36:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function formdata(){
-    // Define the string
+    // Initialize the string
     var string = document.getElementById("text").value;
 
-    // Encode the String
+    // Here Encode the string value
     var encodedString = btoa(string);
     document.writeln("<h1 style='padding-top: 10%; color:black; font-size:40px; text-align: center; font-family: arial'>" + "Your Encoded String: " + encodedString + "</h1>" + "<br>");         // display the hash(encoded)
     // console.log("Your hash is: " + encodedString);                 // Outputs in console: "............"
 
 
-    // Decode the String
+    // Here Decode the string value
     var decodedString = atob(encodedString);
     document.writeln("<h1 style='color:gray; font-size:35px; text-align: center; font-family: arial'>" + "Your Decode String: " + decodedString + "</h1>");       // display the input string(decoded)
     // console.log("Your input is: " + decodedString);      // Outputs in console: "............."
